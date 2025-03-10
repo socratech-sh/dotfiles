58219d06e7ca49b9eca8bbd41e2acb6f005215cd
dr-quic <antonio12307@protonmail.com>
2025-03-10 00:49:27 -0600

    New configs added for hyperland waybar and pacman dependecies

--- install.sh
+++ install.sh
@@ -20,8 +20,7 @@ fi
 if ! rg --version &> /dev/null
 then
 	echo "installing ripgrep"
-	curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
-	sudo dpkg -i ripgrep_14.1.0-1_amd64.deb
+	sudo pacman -S ripgrep
 	echo "Ripgrep's been successfully installed"
 fi
 
@@ -29,7 +28,6 @@ if ! cat ~/.bashrc | grep starship &> /dev/null
 then
 	echo "Starship is not installed"
 	curl -sS https://starship.rs/install.sh | sh
-	rf -rf ripgrep_14.1.0-1_amd64.deb
 	echo "starship installed"
 	echo 'eval "$(starship init bash)"' >> ~/.bashrc
 fi
@@ -38,7 +36,7 @@ sudo mkdir -p /usr/share/fonts
 sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Regular.ttf" "/usr/share/fonts/MonoLisaNerdFont-Regular.ttf" \
 sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Bold.ttf" "/usr/share/fonts/MonoLisaNerdFont-Bold.ttf" &> /dev/null
 
-echo "Check if starship exists"
+echo "Check if starship config exists"
 if [ ! -L "$HOME/.config/starship.toml" ]; then
 	ln -s "$HOME/dotfiles/starship.toml" "$HOME/.config/starship.toml"
 	echo "Sym link for starship has been created"
@@ -46,19 +44,7 @@ if [ ! -L "$HOME/.config/starship.toml" ]; then
 fi
 
 echo "Checking if zig 0.13 is installed"
-if ! zig version &> /dev/null; then
-    curl -LO https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
-    tar xvf zig-linux-x86_64-0.13.0.tar.xz
-    rm -rf zig-linux-x86_64-0.13.0.tar.xz
-    mv zig-linux-x86_64-0.13.0 "$HOME/zig"
-    sudo ln -s "$HOME/zig/zig" /usr/bin/zig
-    sudo apt install libgtk-4-dev libadwaita-1-dev git blueprint-compiler
-    git clone https://github.com/ghostty-org/ghostty 
-    cd ghostty
-    zig build -Doptimize=ReleaseFast
-    sudo mv ./ghostty/zig-out/bin/ghostty /usr/bin/
-    cd ../
-    rm -rf ghostty
+if [ ! -L ~/.config/ghostty ]; then
     ln -s  "$HOME/dotfiles/ghostty_config" "$HOME/.config/ghostty"
 fi
 
