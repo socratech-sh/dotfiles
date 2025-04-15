4adf0db1d032961d2abc0e29365893b8484586c8
dr-quic <antonio12307@protonmail.com>
2025-04-15 14:51:00 -0600

    New install script and gitignore added

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
+echo "Installing fonts"
+sudo mkdir -p /usr/share/fonts
+sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Regular.ttf" "/usr/share/fonts/MonoLisaNerdFont-Regular.ttf" \
+sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Bold.ttf" "/usr/share/fonts/MonoLisaNerdFont-Bold.ttf" &> /dev/null
 
-if ! nvim -v &> /dev/null 
+if ! ghostty --version &> /dev/null
 then
-	echo "You don't have neovim"
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-	sudo rm -rf /opt/nvim
-	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-	rm -rf nvim-linux-x86_64.tar.gz
-	echo 'PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc 
+	echo "Installing ghostty"
+	sudo ln -s "$HOME/dotfiles/ghostty_config" "$HOME/.config/ghostty"
+	echo 'export PS1="\[\e[38;5;35m\]╭─\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\A\[
+e[m\]\[\e[38;5;35m\]-\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\j\[\e[m\]\[
+e[38;5;35m\]-\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\H\[\e[m\]\[\e[38;5;
+5m\]-\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\w\[\e[m\]\[\e[38;5;35m\])\[
+─ |\[\e[m\] "' >> ~/.bashrc
+	pacman -S ghostty
 	source ~/.bashrc
-	echo "Neovim's been successfully installed" 
-fi 
+	echo "Ghostty has been succesfully installed"
+fi
 
-if [ ! -L "$HOME/.config/nvim" ]; then
-	echo "Creating sym link for neovim"
-	ln -s "$(pwd)/nvim" "$HOME/.config/nvim"
+if ! Hyprland --version
+then
+	echo "Installing Hyperland"
+	sudo ln -s "$HOME/dotfiles/hypr" "$HOME/.config/hypr"
+	sudo pacman -S hyprland uwsm
+	
+	echo "if uwsm check may-start; then
+		exec uwsm start hyprland-uwsm.desktop
+	fi" >> ~/.bashrc
 fi
 
 if ! rg --version &> /dev/null
@@ -23,27 +35,21 @@ then
 	echo "Ripgrep's been successfully installed"
 fi
 
-if ! cat ~/.bashrc | grep starship &> /dev/null
+if ! emacs --version &> /dev/null
 then
-	echo "Starship is not installed"
-	curl -sS https://starship.rs/install.sh | sh
-	echo "starship installed"
-	echo 'eval "$(starship init bash)"' >> ~/.bashrc
+	echo "Installing emacs"
+	pacman -S emacs
+	sudo ln -s "$HOME/dotfiles/hypr" "$HOME/.config/doom"
+	git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+	~/.config/emacs/bin/doom install
+	doom sync
 fi
 
-sudo mkdir -p /usr/share/fonts
-sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Regular.ttf" "/usr/share/fonts/MonoLisaNerdFont-Regular.ttf" \
-sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Bold.ttf" "/usr/share/fonts/MonoLisaNerdFont-Bold.ttf" &> /dev/null
-
-echo "Check if starship config exists"
-if [ ! -L "$HOME/.config/starship.toml" ]; then
-	ln -s "$HOME/dotfiles/starship.toml" "$HOME/.config/starship.toml"
-	echo "Sym link for starship has been created"
-	echo "STARSHIP_CONFIG=$HOME/.config/starship.toml" >> ~/.bashrc
+if ! firefox --version
+then
+	echo "Downloading Firefox installing it depends on you"
+	curl --location "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" --output Firefox.tar.bz2
 fi
 
-echo "Checking if zig 0.13 is installed"
-if [ ! -L ~/.config/ghostty ]; then
-    ln -s  "$HOME/dotfiles/ghostty_config" "$HOME/.config/ghostty"
-fi
-
+echo "Installing missing dependecies"
+sudo pacman -S --needed - < pacman.txt
