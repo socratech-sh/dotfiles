5bdc8d3d31b1e5714d72d66c2df6b6d09032763f
socratech-sh <antonio.ruv73@gmail.com>
2025-04-16 10:58:12 -0600

    Install script improvements and dependecies added to pacman.txt

--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
 echo "Installing fonts"
 sudo mkdir -p /usr/share/fonts
-sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Regular.ttf" "/usr/share/fonts/MonoLisaNerdFont-Regular.ttf" \
-sudo ln -s "$HOME/dotfiles/fonts/MonoLisaNerdFont-Bold.ttf" "/usr/share/fonts/MonoLisaNerdFont-Bold.ttf" &> /dev/null
+sudo ln -s $HOME/dotfiles/fonts/MonoLisaNerdFont-Regular.ttf /usr/share/fonts/MonoLisaNerdFont-Regular.ttf \
+sudo ln -s $HOME/dotfiles/fonts/MonoLisaNerdFont-Bold.ttf /usr/share/fonts/MonoLisaNerdFont-Bold.ttf &> /dev/null
 
 if ! ghostty --version &> /dev/null
 then
 	echo "Installing ghostty"
 	sudo ln -s "$HOME/dotfiles/ghostty_config" "$HOME/.config/ghostty"
-	echo 'export PS1="\[\e[38;5;35m\]╭─\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\A\[
-e[m\]\[\e[38;5;35m\]-\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\j\[\e[m\]\[
-e[38;5;35m\]-\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\H\[\e[m\]\[\e[38;5;
-5m\]-\[\e[m\]\[\e[38;5;35m\](\[\e[m\]\[\e[38;5;38m\]\w\[\e[m\]\[\e[38;5;35m\])\[
-─ |\[\e[m\] "' >> ~/.bashrc
 	pacman -S ghostty
 	source ~/.bashrc
 	echo "Ghostty has been succesfully installed"
@@ -25,7 +20,7 @@ then
 	
 	echo "if uwsm check may-start; then
 		exec uwsm start hyprland-uwsm.desktop
-	fi" >> ~/.bashrc
+fi" >> ~/.bashrc
 fi
 
 if ! rg --version &> /dev/null
@@ -39,9 +34,9 @@ if ! emacs --version &> /dev/null
 then
 	echo "Installing emacs"
 	pacman -S emacs
-	sudo ln -s "$HOME/dotfiles/hypr" "$HOME/.config/doom"
+	sudo ln -s $HOME/dotfiles/hypr $HOME/.config/doom
 	git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-	~/.config/emacs/bin/doom install
+	home/socratech/.config/emacs/bin/doom install
 	doom sync
 fi
 
@@ -49,6 +44,10 @@ if ! firefox --version
 then
 	echo "Downloading Firefox installing it depends on you"
 	curl --location "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" --output Firefox.tar.bz2
+	tar xvf Firefox.tar.bz2
+	rm Firefox.tar.bz2
+	mv firefox $HOME/
+	sudo ln -s $HOME/firefox/firefox /usr/bin/firefox
 fi
 
 echo "Installing missing dependecies"
